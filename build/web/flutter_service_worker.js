7fc08741ccc59c26098840fc0fc80af7141ac388
Aman Chaudhary <143288650+aman-chaudhary1@users.noreply.github.com>
2026-05-15 01:04:30 +0530

    commit
    commit

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -65,7 +65,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b6126e72be22025d1a2e321c14ddbb4b
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "1bd675e78c10e54bef324cfbf0a9a0cb",
+"flutter_bootstrap.js": "25881f6b3edd64949bef8147e6c0716b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "5fbf99cadf7418a97e3f6966860785ed",
